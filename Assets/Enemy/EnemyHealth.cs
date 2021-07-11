007398a82272764f9b282436800d646cbf162213
georgebelll <georgebell.com@gmail.com>
2021-07-11 19:28:01 +0100

    First half of the tower defence game. Will soon be improved with path finding and other features

---
+++ Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHitPoints = 5;
+
+    [Tooltip("Adds to max hitpoints when enemy dies")]
+    [SerializeField] int difficultyRamp = 1;
+    
+    int currentHitPoints = 0;
+
+    Enemy enemy;
+
+    void OnEnable()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit(); 
+    }
+
+    void ProcessHit()
+    {
+        currentHitPoints--;
+
+        if (currentHitPoints <= 1)
+        {
+            enemy.RewardGold();
+            maxHitPoints += difficultyRamp;
+            gameObject.SetActive(false);
+        }
+    }
+
+}
